88e58df97bb8eafda91083447c2156dfd959bb36
adamauthor <ahmediarolzasov@gmail.com>
2024-03-17 00:33:14 +0500

    start creating bot

--- main.go
+++ main.go
@@ -1,35 +1,52 @@
 package main
 
 import (
-	"log"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("6823752020:AAFDVQ_jlSnQrdyrhScnGGMEH8a_YA7nGqU")
+	log := logrus.New()
+
+	tgbotapi.SetLogger(log)
+
+	bot, err := tgbotapi.NewBotAPI("6791189994:AAHhBbR36kIx4iRzi51kyWZZJBaR0em5FX4")
 	if err != nil {
-		logrus.Panic(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
 
-	logrus.Infof("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		if update.Message == nil {
+			continue
 		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		if update.Message.Chat.Type != "group" && update.Message.Chat.Type != "supergroup" {
+			msg.Text = "Я не могу нихуя делать. Даун, добавь сначала меня в группу!"
+			bot.Send(msg)
+			continue
+		}
+
+		switch update.Message.Command() {
+		case "start":
+			msg.Text = "Начинаем запуск!"
+		default:
+			msg.Text = "Дебил, я не знаю что это за команда!!!"
+		}
+
+		bot.Send(msg)
 	}
 }

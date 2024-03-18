a3dd91044ec69c83f427aa1466829bcb44815a77
kbeisenb <kbeisenb@L02R01S01s-iMac.local>
2024-03-18 06:00:51 +0500

    refactoring parse of Json file and writing POST request with getting and sending json file

--- llm/post.py
+++ llm/post.py
@@ -1,18 +1,29 @@
 from flask import Flask, request, jsonify, send_file
-from sayaBot import model
+from sayaBot import *
 
 app = Flask(__name__)
 
+# @app.route('/process_json', methods=['POST'])
+# def process_json():
+#     try:
+#         # Запускаем модель для обработки JSON-файла
+#         response_data = model('example.json')
+
+#         # Возвращаем созданный JSON-файл
+#         return send_file(response_data), 200
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 400
+
 @app.route('/process_json', methods=['POST'])
 def process_json():
-    try:
-        # Запускаем модель для обработки JSON-файла
-        response_data = model('example.json')
-
-        # Возвращаем созданный JSON-файл
-        return send_file(response_data), 200
-    except Exception as e:
-        return jsonify({"error": "asd" + str(e)}), 400
+    if request.method == "POST":
+        try:
+            json_data = request.json
+            response_data = model(json_data)
+            # Возвращаем созданный JSON-файл
+            return jsonify({"success": True, "response_data": response_data}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
--- llm/sayaBot.py
+++ llm/sayaBot.py
@@ -5,20 +5,34 @@ import os
 import json
 import requests
 
+def get_args_json(json_data):
+    """
+    This function parses json file
+    """
+    contents = []
+    time = []
+    names = []
+    chat_id = None
+    prompt = ""
 
-def parse_json(filename: str) -> str:
-    with open (filename, "r", encoding="utf-8") as file:
-        json_data = file.read()
+    try:
+        messages = json_data.get("messages", [])  # Получаем список сообщений из JSON данных
 
-    parse_data = json.loads(json_data)
-    messages_json = parse_data["messages"]
-    messages = []
-    leng = len(messages_json)
-    
-    for i in range(leng):
-        messages.append("От: " + messages_json[i]["username"] + ";" + " Текст сообщения: " + messages_json[i]["content"]+ ";" + " Время отпарвки: " + messages_json[i]["time"])
+        for message in messages:
+            content = message.get("content")
+            message_time = message.get("time")
+            name = message.get("username")
+            chat_id = message.get("chat-id")
 
-    return messages
+            contents.append("От: " + name + ";" + " Текст сообщения: " + content + ";" + " Время отпарвки: " + message_time)
+            time.append(message_time)
+            names.append(name)
+
+        return contents, time, names, chat_id
+
+    except Exception as e:
+        print("Ошибка при разборе JSON:", e)
+        return None, None, None, None
 
 def create_response(messages:str, chat:ChatSession, safety_settings=None, generation_config=None) -> str:
     first_prompt = "Я тебе сейчас передам сообщения по одному. Не генерируй ответ до момента пока я не передам тебе START_PEREPHRASE. После этого сделай краткий перессказ полученной беседы. Пересказ должен быть емким, его цель: краткая выжимка всей беседы, и выжимка должна быть короче самой беседы."
@@ -38,43 +52,33 @@ def create_response(messages:str, chat:ChatSession, safety_settings=None, genera
     final_prompt = chat.send_message("START_PEREPHRASE", safety_settings=safety_settings, generation_config=generation_config)
     return final_prompt.text
 
-def load_json(filename, text):
-    with open (filename, "r", encoding="utf-8") as file:
-        json_data = file.read()
-
-    parse_data = json.loads(json_data)
-    messages_json = parse_data["messages"]
-    leng = len(messages_json)
-    time = []
-    
-    for i in range(leng):
-        time.append(messages_json[i]["time"])
-    
+def load_json(time, chat_id, text):
     time_start = min(time)
     time_end = max(time)
-    chat_id = messages_json[0]["chat-id"]
-
 
     data = {
-        "response":
-            {
-                "retelling": text,
-                "time-start": time_start,
-                "time-end": time_end,
-                "chat-id": chat_id
-            }
+        "response": {
+            "retelling": text,
+            "time-start": time_start,
+            "time-end": time_end,
+            "chat-id": chat_id
+        }
     }
 
-    with open("response.json", "w", encoding="utf-8") as json_file:
+    # Генерация уникального имени для файла
+    file_name = "response.json"
+
+    with open(file_name, "w", encoding="utf-8") as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=2)
-    return "response.json"
+    
+    return data
 
 def genai_configure(api_key:str):
     load_dotenv()
     GOOGLE_API_KEY = os.getenv(api_key)
     genai.configure(api_key=GOOGLE_API_KEY)
 
-def model(filename:str):
+def model(json_data):
     genai_configure("GOOGLE_API_KEY")
     model = genai.GenerativeModel('gemini-pro')
 
@@ -99,14 +103,14 @@ def model(filename:str):
         },
     ]
     chat = model.start_chat(history=[])
-    messages = parse_json(filename)
+    messages, time, names, chat_id = get_args_json(json_data)
     response = create_response(messages, chat, safe, config)
-    response_data = load_json(filename, response)
+    response_data = load_json(time, chat_id, response)
     return response_data
 
 
 def main():
     model("example.json")
-    
+
 if __name__ == "__main__":
     main()
